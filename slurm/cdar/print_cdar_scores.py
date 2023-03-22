a347452ebce78110363bf749c1494bb526ea384e
Hyogun <gunsbrother@khu.ac.kr>
2023-03-23 02:51:15 +0900

    Edit: print_cdar_scores Pivot Table Mode

--- slurm/cdar/print_cdar_scores.py
+++ slurm/cdar/print_cdar_scores.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import re
+import os
 import argparse
 
+import numpy as np
 import pandas as pd
-import os
 
 
 only_best_children = True
@@ -29,7 +30,12 @@ parser = argparse.ArgumentParser('')
 parser.add_argument('-p', '--show-path', action='store_true')
 parser.add_argument('-P', '--parsable', action='store_true')
 parser.add_argument('-k', '--show-kmeans', action='store_true')
+parser.add_argument('-v', '--in-pivot', action='store_true')
 args = parser.parse_args()
+
+print_args = ['display.precision', 1, 'max_colwidth', None]
+if args.parsable:
+    print_args += ['display.multi_sparse', False]
 
 orders = {
     'task': {},
@@ -41,17 +47,17 @@ records = []
 for p_pkl in p.rglob('best_pred.pkl'):
     task, subtask, model, add_on, extra_setting, jobname, job_array_idx, *_ = p_pkl.parts[p_pkl.parts.index('cdar')+1:]
     order, task = task.split('_', 1)
-    orders['task'][task] = int(order)
+    orders['task'][task] = str(order)  # will be sorted by dictionary order rather than arithmetic order
     order, subtask = subtask.split('_', 1)
-    orders['subtask'][subtask] = int(order)
+    orders['subtask'][subtask] = str(order)
     order, model = model.split('_', 1)
-    orders['model'][model] = int(order)
+    orders['model'][model] = str(order)
     settings = {
         'task': task,
         'subtask': subtask,
         'model': model,
-        'add_on': add_on,
-        'extra_setting': extra_setting
+        'option1': add_on,
+        'option2': extra_setting
     }
     p_log = next(p_pkl.parent.glob('*.log'))
     with p_log.open() as f:
@@ -85,28 +91,42 @@ sort_bys = ['task', 'subtask', 'model']
 for sort_by in sort_bys:
     df[sort_by+'_order'] = df[sort_by].map(orders[sort_by])
 
-if latex_view:
-    sort_by_metrics = ['gcd_v2', 'gcd_v2_old', 'gcd_v2_new']
-    df = df.sort_values(
-        by=list(map(lambda s: f'{s}_order', sort_bys)) + sort_by_metrics,
-        ascending=[True]*len(sort_bys)+[False]*len(sort_by_metrics)
-    )
-    df = df.groupby(['task', 'subtask', 'model']).head(1)
-    df = df[sort_bys + sort_by_metrics]
-    # df = df.set_index('model')
-    for unique_task in df.task.unique():
-        df_ = df[df.task == unique_task]
-        del df_['task']
-        # df_ = df_.
-        with pd.option_context('display.precision', 1):
-            print(df_)
+sort_by_metrics = [
+    'gcd_v2_balanced', 'gcd_v2_balanced_old', 'gcd_v2_balanced_new',
+    'gcd_v2', 'gcd_v2_old', 'gcd_v2_new',
+]
+
+if args.in_pivot:
+    df = df.sort_values(['gcd_v2_balanced'], ascending=False).groupby(['task', 'subtask', 'model']).head(1)
+    for task in df['task'].unique():
+        print(task)
+        df_task = df[df['task']==task]
+        df_task = df_task.set_index('model')
+        df_task = df_task.sort_values(
+            by=list(map(lambda s: f'{s}_order', sort_bys)) + sort_by_metrics + ['jid'],
+            ascending=[True]*len(sort_bys)+[False]*len(sort_by_metrics)+[True]).fillna('-')
+        # reshaping data
+        df_task = df_task.reset_index()
+        df_task = pd.melt(
+            df_task,
+            id_vars=['model', 'task', 'subtask', 'option1', 'option2'],
+            value_vars=sort_by_metrics[:3],
+            var_name='metric'
+        )  # unpivot
+        df_average = df_task.pivot_table(values='value', index=['model'], columns=['metric'], aggfunc=np.mean, sort=False)
+        df_average = pd.concat([df_average], keys=['Average' + ' '*(len(df_task['subtask'][0])-len('ALL(B)'))], names=['subtask'], axis=1)  # insert level 0 column
+        df_task = df_task.pivot_table(values='value', index=['model'], columns=['subtask', 'metric'], sort=False)
+        df_task = df_task.sort_index(key=lambda models: models.map(orders['model']))
+        df_task = pd.concat([df_task, df_average], axis=1)
+        df_task = df_task.rename(columns=lambda s: s.replace('_', ' → '), level=0)
+        df_task = df_task.rename(columns=dispmap, level=1)
+        with pd.option_context(*print_args):
+            print(df_task)
+        print()
+        print()
 
 else:
-    sort_by_metrics = [
-        'gcd_v2_balanced', 'gcd_v2_balanced_old', 'gcd_v2_balanced_new',
-        'gcd_v2', 'gcd_v2_old', 'gcd_v2_new',
-    ]
-    df = df.set_index(['task', 'subtask', 'model', 'add_on', 'extra_setting'])
+    df = df.set_index(['task', 'subtask', 'model', 'option1', 'option2'])
     df = df.sort_values(
         by=list(map(lambda s: f'{s}_order', sort_bys)) + sort_by_metrics + ['jid'],
         ascending=[True]*len(sort_bys)+[False]*len(sort_by_metrics)+[True]).fillna('-')
@@ -128,8 +148,5 @@ else:
         cols += ['jid']
     df = df[cols]
     df = df.rename(columns=dispmap)
-    print_args = ['display.precision', 1, 'max_colwidth', None]
-    if args.parsable:
-        print_args += ['display.multi_sparse', False]
     with pd.option_context(*print_args):
         print(df)
